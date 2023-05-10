c4d019cc0195087e80975426495a72230ceb61ed
Rexus 17 <94911386+Rexus17@users.noreply.github.com>
2023-05-10 16:41:57 +0630

    Add files via upload

---
+++ pos3/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My Navbar</title>
+	<!-- From Google Fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	
+	<nav>
+		<div class="logo">
+			<h4>Sandhika Galih</h4>
+		</div>
+		<ul>
+			<li><a href="home.php">HOME</a></li>
+			<li><a href="gudang.php">STOK BARANG</a></li>
+		</ul>
+		<div class="menu-toggle">
+			<input type="checkbox">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</nav>
+
+	<div class="container">
+		
+	</div>
+
+<script src="script.js"></script>
+
+</body>
+</html>
