e510e9dd3a5b484a462ad6401c13d28aa27a9202
Rexus 17 <94911386+Rexus17@users.noreply.github.com>
2023-05-13 19:43:58 +0630

    Add files via upload

---
+++ ProjectInventory/src/Koneksi1/Koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Koneksi1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author RIFAI
+ */
+public class Koneksi {
+    
+    Connection koneksi;
+    
+    public static Connection Koneksi() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/inventory", "root", "");
+            
+            return koneksi;
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            
+            return null;
+        }
+    }
+    
+}
