402b5451cc3e6c877c1d0ab0973291902890eb85
Rexus 17 <94911386+Rexus17@users.noreply.github.com>
2023-05-13 19:31:45 +0630

    Add files via upload

---
+++ Nested_Looping/src/Pengulangan_Bersarang/Back_Left_Pyramid.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pengulangan_Bersarang;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author RIFAI
+ */
+public class Back_Left_Pyramid 
+{
+    
+    public static void main(String[] args) 
+    {
+        // membuat scanner baru
+        Scanner input = new Scanner(System.in);
+        
+        // tampilkan output ke user
+        System.out.print("Masukan Jumlah Bintang : ");
+        
+        // untuk menyimpan dan mengirim dari user
+        int jumlah = input.nextInt();
+        
+        // pengulangan untuk banyaknya angka
+        for (int i = 0; i <= 5; i++) 
+        {
+            for (int j = 4; j >= i; j--) 
+            {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ Nested_Looping/src/Pengulangan_Bersarang/Right_Star_Nested_Loop.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *
+ * Materi tanggal : Kamis, 29 - September - 2022
+ *
+ */
+package Pengulangan_Bersarang;
+
+// import library
+import java.util.Scanner;
+
+/**
+ * 
+ * @author RIFAI
+ */
+public class Right_Star_Nested_Loop 
+{
+    
+    public static void main(String[] args) 
+    {      
+        // membuat scanner baru
+        Scanner keyboard = new Scanner(System.in);
+        
+        // tampilkan output ke user 
+        System.out.print("Masukan Jumlah Bintang : ");
+        
+        // untuk menyimpan dan mengirim masukan dari user 
+        int angka = keyboard.nextInt();
+        
+        // pengulangan untuk banyak nya angka
+        for (int i = 1; i <= angka; i++) 
+        {
+            // menghitung banyak nya string kosong dari besar ke kecil
+            int space = angka - i;
+            for (int x = space; x >= 1; x--) 
+            {
+                System.out.print(" ");
+            }
+            
+             // menghitung banyak nya string kosong dari kecil ke besar
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
